3c8e3480abd66a57e798eeab9eeb3e3ee6f06984
Bugale Bugalit <bugalit@gmail.com>
2021-12-19 03:15:23 +0200

    Bump version to 0.1.0 (#2)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup  # type: ignore
 
 setup(
     name='python-lint',
-    version='0.0.1',
+    version='0.1.0',
     url='https://github.com/bugale/python-lint',
     license='MIT',
     author='Bugale',
