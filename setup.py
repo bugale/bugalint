630f6d4e91848ff928797696f2feedf89eda3dc9
Bugale Bugalit <bugalit@gmail.com>
2021-12-19 19:54:03 +0200

    Install missing types for mypy (#4)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup  # type: ignore
 
 setup(
     name='bugalint',
-    version='1.0.0',
+    version='1.1.0',
     url='https://github.com/bugale/bugalint',
     license='MIT',
     author='Bugale',
