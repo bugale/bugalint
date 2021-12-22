172eed9e8b167781525de3ae0dae32baeb677aab
Bugale Bugalit <bugalit@gmail.com>
2021-12-22 05:17:59 +0200

    Support configuration file (#6)

--- bugalint/cli.py
+++ bugalint/cli.py
@@ -10,7 +10,9 @@ import tempfile
 import asyncio
 import logging
 import json
-from typing import Any, NoReturn, Optional, Dict, List
+import configparser
+import glob
+from typing import Any, Coroutine, Iterator, NoReturn, Optional, Dict, List, Tuple
 
 logger = logging.getLogger('bugalint')
 
@@ -33,13 +35,13 @@ class Linter:
     cmdline: str
     regex: str
 
-    async def lint(self, cmdline_args: Dict[str, Any]) -> List[Issue]:
+    async def lint(self, identifier: str, cmdline_args: Dict[str, Any]) -> List[Issue]:
         """Run the linter and return a list of issues"""
         cmdline = self.cmdline.format(**cmdline_args)
-        logger.debug(f'{self.name} cmdline: {cmdline}')
+        logger.debug('%s %s cmdline: %s', self.name, identifier, cmdline)
         proc = await asyncio.create_subprocess_shell(cmdline, shell=True, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
         stdout, stderr = await proc.communicate()
-        logger.debug(f'{self.name} wrote:\nstdout: {stdout!r}\nstderr: {stderr!r}, return code: {proc.returncode}')
+        logger.debug('%s %s wrote:\nstdout: %s\nstderr: %s, return code: {proc.returncode}', self.name, identifier, stdout, stderr)
         cregex = re.compile(self.regex)
         issues: List[Issue] = []
         for line in stdout.decode('utf-8').splitlines():
@@ -68,33 +70,53 @@ def lintly(issues: List[Issue]) -> None:
                    input=json.dumps(issues_json), text=True)
 
 
+def sections_starting_with(config: configparser.ConfigParser, prefix: str) -> Iterator[Tuple[str, configparser.SectionProxy]]:
+    """Returns all sections starting with the prefix"""
+    for section_name in config.sections():
+        if section_name.startswith(prefix):
+            yield section_name[len(prefix):], config[section_name]
+
+
+def get_linters_from_config(config: configparser.ConfigParser) -> Dict[str, Linter]:
+    """Parses the configuration and returns all linters discovered"""
+    linters: Dict[str, Linter] = {}
+    for linter_name, linter_config in sections_starting_with(config, 'bugalint.linters.'):
+        linters[linter_name] = Linter(name=linter_name, cmdline=linter_config['cmdline'], regex=linter_config['regex'])
+        logger.debug('Added linter %s: cmdline=%s, regex=%s', linter_name, linter_config['cmdline'], linter_config['regex'])
+    return linters
+
+
+def initiate_lints(config: configparser.ConfigParser, linters: Dict[str, Linter]) -> List[Coroutine[None, None, List[Issue]]]:
+    """Initiates the lints"""
+    lints: List[Coroutine[None, None, List[Issue]]] = []
+    for lint_name, lint_config in sections_starting_with(config, 'bugalint.lints.'):
+        for path in glob.iglob(lint_config['path'], recursive=True):
+            logger.info('Linting %s', path)
+            cmdline_args = {'path': path, 'py_files': ' '.join((f'"{file}"' for file in glob.glob(os.path.join(path, '**', '*.py'), recursive=True)))}
+            for linter_name, linter in linters.items():
+                if lint_config.get(linter_name):
+                    lints.append(linter.lint(identifier=lint_name, cmdline_args=cmdline_args))
+    return lints
+
+
 async def amain(argv: List[str]) -> int:
     """Run linting tools on the code"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path', default='.')
+    parser.add_argument('--config', default='.bugalint.ini', help='The config file to use')
+    parser.add_argument('--verbose', action='store_true')
     parser.add_argument('--lintly', action='store_true')
     option = parser.parse_args(argv[1:])
 
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG if option.verbose else logging.INFO)
 
-    linters = [
-        Linter(name='mypy', cmdline='"{exe}" -m mypy --show-error-codes --show-column-numbers --no-color-output --no-error-summary "{path}"',
-               regex='(?P<file>.*\\.py):(?:(?P<line>\\d*):)?(?:(?P<col>\\d*):)? [^:]*: (?P<text>.*)'),
-        Linter(name='pylint', cmdline='"{exe}" -m pylint {files}', regex='(?P<file>[^:]*):(?P<line>\\d*):(?P<col>\\d*): (?P<code>[^:]*): (?P<text>.*)'),
-        Linter(name='flake8', cmdline='"{exe}" -m flake8 "{path}"', regex='(?P<file>[^:]*):(?P<line>\\d*):(?P<col>\\d*): (?P<code>[^ ]*) (?P<text>.*)'),
-    ]
+    config = configparser.ConfigParser()
+    config.read([os.path.join(os.path.dirname(__file__), 'defaults.ini'), option.config])
+    linters = get_linters_from_config(config=config)
+    lints = initiate_lints(config=config, linters=linters)
 
-    def has_dot(path: str) -> bool:
-        return any(part.startswith('.') and part not in ('.', '..') for part in path.split(os.path.sep))
-
-    files = [os.path.join(root, file) for root, _, files in os.walk(option.path) for file in files if file.endswith('.py') and not has_dot(root)]
-    cmdline_args = {'exe': sys.executable, 'path': option.path, 'files': ' '.join((f'"{file}"' for file in files))}
-
-    issues: List[Issue] = sum(list(await asyncio.gather(*[linter.lint(cmdline_args) for linter in linters])), start=[])
-
+    issues: List[Issue] = sum(list(await asyncio.gather(*lints)), start=[])
     for issue in issues:
         logger.error(repr(issue))
-
     if not issues:
         logger.info('No issues found :)')
 
